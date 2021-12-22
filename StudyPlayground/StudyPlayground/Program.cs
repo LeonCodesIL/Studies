0d99628822f3c6a2fdc8283b253dc5e5cee81bd0
LeonCodesIL <leonkukkerpro@gmail.com>
2021-12-22 17:18:08 +0200

    adding my console application

---
+++ StudyPlayground/StudyPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudyPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
