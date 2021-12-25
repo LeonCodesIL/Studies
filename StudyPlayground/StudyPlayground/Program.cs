2561100918baa005a4a2657d669551981248f05a
LeonCodesIL <leonkukkerpro@gmail.com>
2021-12-25 15:47:50 +0200

    simple string problem in c

--- StudyPlayground/StudyPlayground/Program.cs
+++ StudyPlayground/StudyPlayground/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-
+using System.IO;
 namespace StudyPlayground
 {
     class Program
     {
         static void Main(string[] args)
         {
-            }
+            string text = File.ReadAllText("../../../TextFile2.txt");
+            
+
+        }
 
     }  
 }
-            
-    
---
+++ StudyPlayground/StudyPlayground/Source.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+void main()
+{
+	char c;
+	int counter, num_of_strings;
+	counter = num_of_strings = 0;
+
+	while ((c = getchar()) != EOF) {
+		printf("%c\n", c);
+		if (c == 'a') {
+			counter = 0;
+			counter++;
+		}
+		else if (c == 'b' && counter == 1) {
+			counter++;
+		}
+		else if (c == 'c' && counter == 2) {
+			counter++;
+		}
+		else { counter = 0; }
+
+		if (counter == 3) {
+			counter = 0;
+			num_of_strings++;
+		}
+	}
+	printf("the string abc has occured %d times.", num_of_strings);
+
+}
+
