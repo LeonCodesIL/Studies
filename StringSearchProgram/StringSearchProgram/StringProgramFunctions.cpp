410d5ddaf81f1415b9a1b46ffd42c54d4e6c4ee2
LeonCodesIL <Leonkukkerpro@gmail.com>
2021-12-28 21:29:44 +0200

    String search program reupload

---
+++ StringSearchProgram/StringSearchProgram/StringProgramFunctions.cpp
@@ -0,0 +1,21 @@
+#include "StringProgramFunctions.h"
+#include <stdio.h>
+
+//for later use;
+//FILE* file;
+//fopen_s(&file, "text.txt", "r");
+//	fclose(file);
+
+int getline(char s[], int lim) {
+	char c, n;
+	n = 0;
+	while ((c = getchar()) != EOF && c != '\n' && n < lim) {
+		s[n] = c;
+		n++;
+	}
+	return n;
+}
+
+int getword(char w[], int lim) {
+
+}
---
+++ StringSearchProgram/StringSearchProgram/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "StringProgramFunctions.h"
+
+
+
+int main()
+{
+	int result;
+	char s[LINE];
+	result = getline(s, LINE);
+	printf("number of chars: %d", result);
+
+
+}
